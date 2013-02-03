48151e99e14a018811ff89b4cbc8dbb955f4ce87
Pavle Jovanovic <pavlej@vailsys.com>
2013-02-03 00:22:53 -0600

    Added so much stuff...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.boxshadow.min
+//= require image_transmission
 //= require_tree .
---
+++ app/assets/javascripts/image_transmission.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function() {
+	jQuery(".picture-list img").click(function() {
+		var img = new Image();
+		img = jQuery(this).attr("src");
+		var dimensions = resize_image_dimensions(img);
+		jQuery("#image-transmission-image").attr("src", jQuery(this).attr("src"));
+		jQuery("#image-transmission-image").css({"width" : dimensions.x+"px", "height" : dimensions.y+"px"});
+		jQuery("#image-transmission").animate({top: "100px"}, "slow", function(){
+			jQuery("#transmission-end").fadeIn();
+		});
+	});
+	jQuery("#transmission-end").click(function() {
+		jQuery("#transmission-end").hide();
+		jQuery("#image-transmission").animate({top: "-400px"}, "slow");
+	});
+});
+
+function Dimensions(x, y) {
+    this.x  = x;
+    this.y = y;
+}
+
+function resize_image_dimensions(image) {
+	var xMax = 944;
+	var yMax = 340;
+	var ratio;
+	var ratioMax;
+	ratio = image.width / image.height;
+	ratioMax = xMax / yMax;
+	//width is larger
+	if (ratio > ratioMax){
+		return new Dimensions(xMax, image.height * xMax / image.width);
+	}
+	else{
+		return new Dimensions(image.width * yMax / image.height, yMax);
+	}
+}
