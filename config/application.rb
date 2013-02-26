e75e4b1fadda96b3c66cc7a3685be6a2541e1f78
Pavle Jovanovic <pavlej@vailsys.com>
2013-02-25 22:47:14 -0600

    Testing movie

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Superjova
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
